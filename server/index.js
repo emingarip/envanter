a71cf74c978e38a797541af9cc73eeb2ba42d630
emingarip <emin@emingarip.com>
2025-06-19 17:26:49 +0300

    Railway deployment icin hazirlik

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
